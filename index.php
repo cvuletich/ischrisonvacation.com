28a9519758e65ee15bad156febe057b528d04295
Chris Vuletich <cvuletich@gmail.com>
2015-03-25 12:10:48 -0500

    Updating with newest content

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
 <?php
 $ischrisonvacation = "no";
-$replies['yes'] = array("Go away.","You can get a hold of me by","Hard work never killed anybody, but why take the chance?");
-$replies['no'] = array("How long until the next one?","Serious case of the Mondays.","Bummer.","Work fascinates me.  I can sit and look at it for hours.","It's not how good your work is, it's how well you explain it.","Not even close...");
+$replies['yes'] = array(
+  "Go away.",
+  "You can get a hold of me by CðÔnT[¹Î Å¤Õn­qe§Ðoi ü\"Ëkq ¶ÙíÍæ",
+  "Hard work never killed anybody, but why take the chance?",
+  "It's not about going on vacation, it's about telling everyone I'm on one.",
+  "I don't always go on vacation, but when I do, I go.",
+  "There is never enough time to do all the nothing you want.",
+  "Brace yourselves, vacation is coming.",
+  "\"Like\" if you need a vacation"
+);
+$replies['no'] = array(
+  "How long until the next one?",
+  "Serious case of the Mondays.",
+  "Bummer.",
+  "Work fascinates me.  I can sit and look at it for hours.",
+  "It's not how good your work is, it's how well you explain it.",
+  "Not even close...",
+  "No man needs a vacation as much as the man who just had one.",
+);
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Is Chris On Vacation?</title>
+    <meta name="keywords" content="chris, vuletich, developer, vacation" />
+    <meta name="description" content="Is Chris on vacation? Find out here!">
+    <meta name="author" content="Chris Vuletich">
+    <link rel="author" href="https://plus.google.com/u/0/+ChrisVuletich" />
     <style>
       html, body {
         background-color: #000;
+        color: #fff;
         font-family: Courier;
       }
       .container {
@@ -48,9 +70,23 @@ $replies['no'] = array("How long until the next one?","Serious case of the Monda
         position: absolute;
         right: 20px;
       }
+      .fb-like {
+        color: #fff;
+        margin: 10px auto;
+        text-align: center;
+        width: 100%;
+      }
     </style>
   </head>
   <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=247461622069412&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
     <div class="container">
       <div class="answer"><?php echo strtoupper($ischrisonvacation); ?></div>
       <div class="small"><?php echo $replies[$ischrisonvacation][rand(0, count($replies[$ischrisonvacation]) - 1)]; ?></div>
@@ -58,6 +94,7 @@ $replies['no'] = array("How long until the next one?","Serious case of the Monda
         <span class="right"><a href="http://chrisvuletich.com">http://chrisvuletich.com</a></span>
       </div>
     </div>
+    <div class="fb-like" data-colorscheme="dark" data-href="http://ischrisonvacation.com" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
     <script type="text/javascript">
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-11868870-11']);
